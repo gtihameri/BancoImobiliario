edaad968efd1bada1e8ca5d408cf76c3382d479b
George <george.tihameri@gmail.com>
2021-06-04 10:00:13 -0300

    Melhora no teste do aleatorio

--- test_aleatorio.py
+++ test_aleatorio.py
@@ -6,15 +6,25 @@ propriedade = Propriedade(60, 5, "Apartamento - Ibirapuera")
 aleatorio = Aleatorio("Maria")
 
 def test_compra_propriedade():
-    saldo_anterior = aleatorio.saldo
-    aleatorio.acao(propriedade)
-    assert  aleatorio.saldo == saldo_anterior  or \
-            aleatorio.saldo == saldo_anterior - propriedade.venda or \
-            aleatorio.saldo == saldo_anterior - propriedade.aluguel
-jogador = Jogador("Carlos")
-propriedade.dono = jogador
+    NUM_TENTATIVAS = 5000
+    resultado = []
+    for i in range(NUM_TENTATIVAS):
+        propriedade = Propriedade(60, 5, "Apartamento - Ibirapuera")
+        aleatorio = Aleatorio("Maria")
+        saldo_anterior = aleatorio.saldo
+        aleatorio.acao(propriedade)
+        if aleatorio.saldo == saldo_anterior - propriedade.venda:
+            # Comprou
+            resultado.append("comprou")
+        else:
+            resultado.append(("Nao Comprou"))
+        compras = len([r for r in resultado if r == "comprou"])/NUM_TENTATIVAS*100
+
+    assert  compras > 48 and compras < 52
 
 def test_aluga_propriedade():
+    jogador = Jogador("Carlos")
+    propriedade.dono = jogador
     saldo_anteriro = aleatorio.saldo
     aleatorio.acao(propriedade)
     assert aleatorio.saldo == saldo_anteriro - propriedade.aluguel
